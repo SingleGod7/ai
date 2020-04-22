99df9a5cbb26b539b03e8e6b6050d20be0cb3927
SingleGod7 <44106892+SingleGod7@users.noreply.github.com>
2020-04-22 23:41:32 +0800

    Add files via upload

---
+++ bo_client.py
@@ -0,0 +1,16 @@
+import socket
+import sys
+
+#客户端部分
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+a = sys.argv
+a = ' '.join(a)
+s.connect(('127.0.0.1',8700))
+s.send(a.encode('utf-8'))
+c = s.recv(1024).decode().split(' ')
+c = [i for i in c if(len(str(i))!=0)]
+print(c[0][1:])
+print(c[1])
+print(c[2])
+print(c[3][:-1])
+
