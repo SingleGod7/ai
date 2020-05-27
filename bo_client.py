581d7ef2efac3465111b7204de75d6d567a9a09b
SingleGod7 <44106892+SingleGod7@users.noreply.github.com>
2020-05-27 15:53:53 +0800

    Update bo_client.py

--- bo_client.py
+++ bo_client.py
@@ -9,8 +9,8 @@ s.connect(('127.0.0.1',8700))
 s.send(a.encode('utf-8'))
 c = s.recv(1024).decode().split(' ')
 c = [i for i in c if(len(str(i))!=0)]
-print(c[0][1:])
+print(c[0])
 print(c[1])
 print(c[2])
-print(c[3][:-1])
-print(c[4])
+print(c[3])
+
