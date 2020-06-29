b82ae8b565779168c5652b41f47b1e931e9ad337
SingleGod7 <44106892+SingleGod7@users.noreply.github.com>
2020-06-30 00:45:44 +0800

    Add files via upload

---
+++ test-file.py
@@ -0,0 +1,27 @@
+import os
+import time 
+
+localtime = time.strftime("%Y-%m-%d-%H-%M-%S",time.localtime())
+if not os.path.exists('data'):
+    os.mkdir('data')
+    f = open('./data/%s-experiment.txt'%localtime,'w')
+
+class Observer():
+    def __init__(self, parameters, waitingTime):
+        self.parameters = parameters
+        self.waitingTime = waitingTime
+
+    def Observe(self):  #定义单次探测过程
+        for i in range(self.parameters):
+            a = os.popen("ls" + self.parameters[i])
+            #加入警告功能
+            f.write(self.parameters[i] + ':' + str(a) + '   ')
+        f.write('\n')
+
+    def start(self):  #定义整体循环
+        while (True):
+            Observe()
+            time.sleep(self.waitingTime)            
+
+Observer1 = Observer()
+Observer1.start() 
